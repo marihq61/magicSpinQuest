4c35dab1aed621964fbc5a5cc7a9479c87a8b1b5
marihq61 <40043479+marihq61@users.noreply.github.com>
2025-06-22 13:38:27 -0500

    cambio boton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,12 @@
 <section class="h-full w-full relative bg-[url('/banner.gif')] bg-cover bg-no-repeat bg-center">
 	<a 
 		href="/play"
-		class="absolute top-[45vh] left-1/2 transform -translate-x-1/2
-			bg-red-700 hover:bg-red-600 text-white text-2xl font-extrabold uppercase tracking-wide
-			py-4 px-8 rounded-2xl shadow-2xl transition duration-300">
-		Let’s Play!
+		class="absolute top-[40vh] lg:top-[45vh] left-1/2 transform -translate-x-1/2
+			bg-white text-black text-md lg:text-2xl font-extrabold uppercase tracking-wide
+			p-3 lg:py-4 lg:px-8 rounded-2xl shadow-2xl transition duration-300
+			flex items-center gap-2">
+			Let’s Play! 
+		<img src="/icons8-casino.gif" alt="" class="w-6 h-6 lg:w-8 lg:h-8">
 	</a>
 </section>
 
